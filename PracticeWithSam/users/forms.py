89824df8d0f38725a8a0b0c418252cfb889afb5d
Sunnykd18 <95135405+Sunnykd18@users.noreply.github.com>
2022-03-26 18:43:05 +0530

    login/Logout/Register feature added

--- PracticeWithSam/users/forms.py
+++ PracticeWithSam/users/forms.py
@@ -3,8 +3,14 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-class UserCreationForm(UserCreationForm):
+class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username','password1','password2']
+        fields = ['username', 'password1', 'password2']
+
+
+class UserLoginForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields =['username','password']
--- PracticeWithSam/users/views.py
+++ PracticeWithSam/users/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
-from .forms import UserCreationForm
+from .forms import UserRegisterForm,UserLoginForm
+from django.contrib import messages
+from django.contrib.auth.hashers import check_password
 
 
 def register(request):
     context = {}
-    form = UserCreationForm(request,)
-    context = {
-        'form': form
-    }
-    return render(request,'users/register.html',context=context)
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username} !')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+        context = {
+            'form': form
+        }
+    return render(request, 'users/register.html', context=context)
+
+
+
